a8ee8fd76db383588ff40fd0b5b8690e9615b757
hmkuklinski <hmkuklinski@gmail.com>
2024-06-20 21:47:39 -0400

    added more fish for bigger pages

--- src/Fishes.js
+++ src/Fishes.js
@@ -170,7 +170,7 @@ export default function Fishes(){
             setImagesPerPage(4);
         }
         else {
-            setImagesPerPage(6);
+            setImagesPerPage(8);
         }
     }, [width]);
 
